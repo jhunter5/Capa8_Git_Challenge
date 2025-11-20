e319fdb97156905cb2e3f0c244eaf38cf198e7d8
jhunter5 <jhunter@unal.edu.co>
2025-11-19 23:19:42 -0500

    Added Memory

--- amazingCalculator.py
+++ amazingCalculator.py
@@ -1,6 +1,18 @@
 class Calculator:
+    def __init__(self):
+        self.history = []
+
     def add(self, a, b):
-        return a + b
+        result = a + b
+        self.history.append(result)
+        return result
 
     def subtract(self, a, b):
-        return a - b
+        result = a - b
+        self.history.append(result)
+        return result
+
+    def previous_result(self):
+        if self.history:
+            return self.history[-1]
+        return None
